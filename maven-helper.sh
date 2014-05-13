scijava/scijava-scripts

commit b54b1e36676516f6995044ee2937334c355fa158

maven-helper: use URLs triggering Nexus to cache
... as opposed to URLs accessing the cached data, if any were cached.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

maven-helper.sh
@@ -8,15 +8,15 @@ set -e
 
 root_url () {
 	test snapshots != "$2" || {
-		if curl -fs http://maven.imagej.net/content/repositories/sonatype-snapshots/"$1"/ > /dev/null 2>&1
+		if curl -fs http://maven.imagej.net/service/local/repositories/sonatype-snapshots/content/"$1"/maven-metadata.xml > /dev/null 2>&1
 		then
-			echo http://maven.imagej.net/content/repositories/sonatype-snapshots
+			echo http://maven.imagej.net/service/local/repositories/sonatype-snapshots/content
 		else
 			echo http://maven.imagej.net/content/repositories/snapshots
 		fi
 		return
 	}
-	echo http://maven.imagej.net/content/groups/public
+	echo http://maven.imagej.net/service/local/repo_groups/public/content
 }
 
 die () {

commit b8500f188a8a919af6c956975df0540270f7f827

maven-helper.sh: remove broken artifacts command
After commit b54b1e36676516f6995044ee2937334c355fa158, the "artifacts"
command stopped working. Since it was only used by sj-hierarchy.pl, and
that script now does the work without assistance from maven-helper, we
can just remove the artifacts command to simplify our lives.

maven-helper.sh
@@ -394,15 +394,6 @@ get_jar () {
 	echo "$tmpfile"
 }
 
-# Given a groupId, get the list of artifacts
-
-list_artifacts () {
-	url="$(root_url)/$(echo "$1" | tr . /)/"
-	links="$(curl -s "$url" | grep "<a href=\"$url.*/</a>")"
-	artifacts="$(echo "$links" | sed 's/ *<[^>]*>\(.*\)\/<\/a>/\1/')"
-	echo "$artifacts"
-}
-
 # Given a GAV parameter, get the commit from the manifest of the deployed .jar
 
 commit_from_gav () {
@@ -471,9 +462,6 @@ is_deployed () {
 # The main part
 
 case "$1" in
-artifacts)
-	list_artifacts "$2"
-	;;
 commit)
 	commit_from_gav "$2"
 	;;
@@ -525,9 +513,6 @@ is-deployed)
 	die "Usage: $0 [command] [argument...]"'
 
 Commands:
-
-artifacts <groupId>
-	Gets the list of artifacts of the given group.
 
 commit <groupId>:<artifactId>:<version>
 	Gets the commit from which the given artifact was built.