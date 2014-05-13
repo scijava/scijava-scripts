33e53c99594d97ea76b7a650627ddda6d4155845
Curtis Rueden <ctrueden@wisc.edu>
2014-05-13 14:24:01 -0500

    Add a simple script to check for needed releases
    It compares the master branch with the release tag of the newest release
    to see if there have been new commits. If so, it reports that.

---
+++ check-release.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/perl
+
+# check-release.pl - checks whether the tip of the master branch
+#                    has changed since the last release.
+
+use strict;
+
+# make sure the latest commits and tags are available
+`git fetch origin master 2> /dev/null`;
+`git fetch --tags 2> /dev/null`;
+
+if (! -e "pom.xml") {
+  print STDERR "[ERROR] No pom.xml: " . `pwd`;
+  exit 1;
+}
+# determine the project's GAV
+my $gav = `maven-helper.sh gav-from-pom pom.xml`;
+chomp $gav;
+my ($groupId, $artifactId, $version) = split(':', $gav);
+my $ga = "$groupId:$artifactId";
+
+# determine the latest release
+my $latest = `maven-helper.sh latest-version \"$ga\"`;
+chomp $latest;
+
+if (!$latest || $latest =~ /\-SNAPSHOT$/) {
+  print STDERR "[ERROR] $ga: No release version\n";
+  exit 2;
+}
+
+# compare the release tag with the master branch
+my $tag = "$artifactId-$latest";
+
+if (!`git tag -l | grep $tag`) {
+  print STDERR "[ERROR] $ga: No release tag: $tag\n";
+  exit 3;
+}
+
+my @commits = `git cherry -v $tag master`;
+my $commitCount = @commits;
+if ($commitCount > 1 ||
+  $commitCount == 1 && $commits[0] !~ /Bump to next development cycle$/)
+{
+  # new commits on master; a release is potentially needed
+  print "$ga: $commitCount commits on master since $latest\n";
+}
