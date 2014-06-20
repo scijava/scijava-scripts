2462ea9b29f50202876c5e746328325e7c0eb41c
Curtis Rueden <ctrueden@wisc.edu>
2014-06-20 16:57:50 -0500

    melting-pot.sh: simplify aggregator POM
    There is no need to extend a parent POM at all, actually.

--- melting-pot.sh
+++ melting-pot.sh
@@ -7,20 +7,13 @@ rm -rf melting-pot
 mkdir melting-pot
 cd melting-pot
 
-psj_version="$(maven-helper.sh latest-version org.scijava:pom-scijava)"
-
 echo '<?xml version="1.0" encoding="UTF-8"?>' > pom.xml
 echo '<project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">' >> pom.xml
 echo '	<modelVersion>4.0.0</modelVersion>' >> pom.xml
 echo >> pom.xml
-echo '	<parent>' >> pom.xml
-echo '		<groupId>org.scijava</groupId>' >> pom.xml
-echo '		<artifactId>pom-scijava</artifactId>' >> pom.xml
-echo "		<version>$psj_version</version>" >> pom.xml
-echo "		<relativePath />" >> pom.xml
-echo '	</parent>' >> pom.xml
-echo >> pom.xml
+echo '	<groupId>org.scijava</groupId>' >> pom.xml
 echo '	<artifactId>melting-pot</artifactId>' >> pom.xml
+echo '	<version>0.0.0-SNAPSHOT</version>' >> pom.xml
 echo '	<packaging>pom</packaging>' >> pom.xml
 echo >> pom.xml
 echo '	<name>SciJava Uber Build</name>' >> pom.xml
