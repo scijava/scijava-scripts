f84d70296deb78d96cbce597a579a0d1f1b20640
Curtis Rueden <ctrueden@wisc.edu>
2017-08-18 16:30:09 -0500

    Add a javadoc update script for use by Travis CI
    This way, we don't have to replicate this logic, and then later
    keep it in sync, across the various SciJava javadoc repositories.

---
+++ travis-javadoc.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+#
+# travis-javadoc.sh - A script to build the javadocs of a SciJava-based project.
+#
+
+if [ "$TRAVIS_SECURE_ENV_VARS" = true \
+  -a "$TRAVIS_PULL_REQUEST" = false \
+  -a "$TRAVIS_BRANCH" = master ]
+then
+  project=$1
+
+  # Build the javadocs.
+  mvn -Pbuild-javadocs &&
+  test -d target/apidocs &&
+
+  # Configure SSH. SSH_PRIVATE_KEY must contain the private
+  # RSA key for communicating with the git remote.
+  mkdir -p "$HOME/.ssh" &&
+  echo "$SSH_PRIVATE_KEY" > "$HOME/.ssh/id_rsa" &&
+  echo 'github.com,192.30.252.130 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==' > "$HOME/.ssh/known_hosts" &&
+
+  # Configure git settings.
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+
+  # Clone the javadoc.scijava.org repository.
+  git clone --quiet --depth 1 git@github.com:scijava/javadoc.scijava.org > /dev/null &&
+
+  # Update the relevant javadocs.
+  cd javadoc.scijava.org &&
+  rm -rf "$project" &&
+  mv ../target/apidocs "$project" &&
+
+  # Commit and push the changes.
+  git add "$project" &&
+  git commit -m "Update $project javadocs (Travis build $TRAVIS_BUILD_NUMBER)" &&
+  git push -q origin gh-pages > /dev/null
+
+  echo "Update complete."
+else
+  echo "Skipping non-canonical branch $TRAVIS_BRANCH"
+fi
