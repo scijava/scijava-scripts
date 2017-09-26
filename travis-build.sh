84ed694dc92c9300fc9ad37cef5bcc7930a699ab
Curtis Rueden <ctrueden@wisc.edu>
2017-09-26 16:04:48 +0200

    travis-build.sh: add feedback on the chosen path
    Just for clarity, it is nice to be explicit about what is happening.

--- travis-build.sh
+++ travis-build.sh
@@ -9,7 +9,9 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = true
   -a "$TRAVIS_PULL_REQUEST" = false
   -a "$TRAVIS_BRANCH" = master ]
 then
+  echo "== Building and deploying master SNAPSHOT =="
   mvn -Pdeploy-to-imagej deploy --settings "$dir/settings.xml"
 else
+  echo "== Building the artifact locally only =="
   mvn install
 fi
