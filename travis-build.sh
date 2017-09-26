4c344d46d810faf51c0109f3a21f6f37c15c1b2e
Curtis Rueden <ctrueden@wisc.edu>
2017-09-26 23:59:25 +0200

    travis-build.sh: suppress download messages
    Otherwise, Travis complains that the log is too long to show.

--- travis-build.sh
+++ travis-build.sh
@@ -4,20 +4,24 @@
 # travis-build.sh - A script to build and/or release SciJava-based projects.
 #
 
+# NB: Suppress "Downloading/Downloaded" messages.
+# See: https://stackoverflow.com/a/35653426/1207769
+export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn
+
 dir="$(dirname "$0")"
 if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
   -a "$TRAVIS_BRANCH" = master ]
 then
   echo "== Building and deploying master SNAPSHOT =="
-  mvn -Pdeploy-to-imagej deploy --settings "$dir/.travis/settings.xml"
+  mvn -B -Pdeploy-to-imagej deploy --settings "$dir/.travis/settings.xml"
 elif [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
   -a -f release.properties ]
 then
   echo "== Cutting and deploying release version =="
-  mvn release:perform
+  mvn -B release:perform
 else
   echo "== Building the artifact locally only =="
-  mvn install
+  mvn -B install
 fi
