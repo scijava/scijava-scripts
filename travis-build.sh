8f102b62325e099b99fc3694e5a830cd74b8c2d7
Curtis Rueden <ctrueden@wisc.edu>
2017-09-26 17:06:33 +0200

    travis-build.sh: look for settings.xml in .travis

--- travis-build.sh
+++ travis-build.sh
@@ -10,7 +10,7 @@ if [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_BRANCH" = master ]
 then
   echo "== Building and deploying master SNAPSHOT =="
-  mvn -Pdeploy-to-imagej deploy --settings "$dir/settings.xml"
+  mvn -Pdeploy-to-imagej deploy --settings "$dir/.travis/settings.xml"
 elif [ "$TRAVIS_SECURE_ENV_VARS" = true \
   -a "$TRAVIS_PULL_REQUEST" = false \
   -a -f release.properties ]
