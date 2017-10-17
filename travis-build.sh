c1b5462a65c217c958e9d658a94aaad490e8a2f8
Curtis Rueden <ctrueden@wisc.edu>
2017-10-17 12:25:15 -0500

    travis-build.sh: use tabs for leading whitespace

--- travis-build.sh
+++ travis-build.sh
@@ -10,18 +10,18 @@ export MAVEN_OPTS=-Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf
 
 dir="$(dirname "$0")"
 if [ "$TRAVIS_SECURE_ENV_VARS" = true \
-  -a "$TRAVIS_PULL_REQUEST" = false \
-  -a "$TRAVIS_BRANCH" = master ]
+	-a "$TRAVIS_PULL_REQUEST" = false \
+	-a "$TRAVIS_BRANCH" = master ]
 then
-  echo "== Building and deploying master SNAPSHOT =="
-  mvn -B -Pdeploy-to-imagej deploy --settings "$dir/.travis/settings.xml"
+	echo "== Building and deploying master SNAPSHOT =="
+	mvn -B -Pdeploy-to-imagej deploy --settings "$dir/.travis/settings.xml"
 elif [ "$TRAVIS_SECURE_ENV_VARS" = true \
-  -a "$TRAVIS_PULL_REQUEST" = false \
-  -a -f release.properties ]
+	-a "$TRAVIS_PULL_REQUEST" = false \
+	-a -f release.properties ]
 then
-  echo "== Cutting and deploying release version =="
-  mvn -B --settings "$dir/.travis/settings.xml" release:perform
+	echo "== Cutting and deploying release version =="
+	mvn -B --settings "$dir/.travis/settings.xml" release:perform
 else
-  echo "== Building the artifact locally only =="
-  mvn -B install
+	echo "== Building the artifact locally only =="
+	mvn -B install
 fi
