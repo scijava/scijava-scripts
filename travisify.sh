53ac7bae63a5f7e70af322c67c76c2d964180537
Curtis Rueden <ctrueden@wisc.edu>
2017-10-22 13:08:27 -0500

    travisify.sh: make the new build.sh executable

--- travisify.sh
+++ travisify.sh
@@ -112,6 +112,7 @@ EOL
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
 sh travis-build.sh
 EOL
+	chmod +x "$tmpFile"
 	update "$travisBuildScript"
 
 	# Remove obsolete Travis-related files.
