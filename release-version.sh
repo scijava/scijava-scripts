scijava/scijava-scripts

commit 600e054e99d9864164b55fea105ee6fb1689209f

release-version.sh: simplify skip-gpg test

release-version.sh
@@ -21,10 +21,8 @@ valid_semver_bump () {
 }
 
 verify_gpg_settings () {
-	if [ ! "$SKIP_GPG" ]
-	then
-		test -f .travis/signingkey.asc.enc ||
-			die 'GPG configuration not found. Please use travisify.sh to add it.
+	test "$SKIP_GPG" -o -f .travis/signingkey.asc.enc ||
+		die 'GPG configuration not found. Please use travisify.sh to add it.
 See also: https://github.com/scijava/pom-scijava/wiki/GPG-Signing'
 	fi
 }

commit fbf53137c86056e5cd0997d5536225fdeed7215d

Fix syntax error introduced by commit 600e054

release-version.sh
@@ -24,7 +24,6 @@ verify_gpg_settings () {
 	test "$SKIP_GPG" -o -f .travis/signingkey.asc.enc ||
 		die 'GPG configuration not found. Please use travisify.sh to add it.
 See also: https://github.com/scijava/pom-scijava/wiki/GPG-Signing'
-	fi
 }
 
 verify_git_settings () {