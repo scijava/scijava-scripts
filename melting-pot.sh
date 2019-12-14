c0308d2ca5ae14b7fa66315658e3a2fd282d0a8a
Wei Ouyang <oeway007@gmail.com>
2019-12-14 18:57:18 +0100

    only include runtime dependencies

--- melting-pot.sh
+++ melting-pot.sh
@@ -412,7 +412,7 @@ retrieveSource() {
 deps() {
 	cd "$1"
 	debug "mvn dependency:list"
-	local depList="$(mvn -B dependency:list)" ||
+	local depList="$(mvn -DincludeScope=runtime -B dependency:list)" ||
 		die "Problem fetching dependencies!" 5
 	echo "$depList" | grep '^\[INFO\]    [^ ]' |
 		sed 's/\[INFO\]    //' | sed 's/  *(optional) *$//' | sort
