scijava/scijava-scripts

commit 90b2b83cb402e562fd857ad59473263ee0e29b54

release-version.sh: fix pom-scijava version check
We can't ask maven.scijava.org anymore, because it no longer
proxies Maven Central since the upgrade to Nexus v3.

So instead, we ask Maven Central directly, which works well.

release-version.sh
@@ -181,13 +181,10 @@ test "$SKIP_VERSION_CHECK" || {
 }
 
 # Check that the project extends the latest version of pom-scijava.
-MAVEN_HELPER="$(cd "$(dirname "$0")" && pwd)/maven-helper.sh"
-test -f "$MAVEN_HELPER" ||
-	die "Missing helper script at '$MAVEN_HELPER'
-Do you have a full clone of https://github.com/scijava/scijava-scripts?"
 test "$SKIP_VERSION_CHECK" -o "$parentGAV" != "${parentGAV#$}" || {
 	debug "Checking pom-scijava parent version"
-	latestParentVersion=$(sh -$- "$MAVEN_HELPER" latest-version "$parentGAV")
+	psjMavenMetadata=https://repo1.maven.org/maven2/org/scijava/pom-scijava/maven-metadata.xml
+	latestParentVersion=$(curl -fsL "$psjMavenMetadata" | grep '<release>' | sed 's;.*>\([^<]*\)<.*;\1;')
 	currentParentVersion=${parentGAV##*:}
 	test "$currentParentVersion" = "$latestParentVersion" ||
 		die "Newer version of parent '$parentGAV' is available: $latestParentVersion.

commit f5482fdb8471824b3aae4acc3fee8cd9927846db

release-version: check version of pom-scijava only
If the parent POM is not pom-scijava, don't worry about it.

This commit fixes a bug introduced in 90b2b83cb4, where the parent POM
version gets checked against the newest org.scijava:pom-scijava release
version, even for parent POMs besides org.scijava:pom-scijava.

release-version.sh
@@ -188,7 +188,7 @@ test "$SKIP_VERSION_CHECK" || {
 }
 
 # Check that the project extends the latest version of pom-scijava.
-test "$SKIP_VERSION_CHECK" -o "$parentGAV" != "${parentGAV#$}" || {
+test "$SKIP_VERSION_CHECK" -o "$parentGroup" != "org.scijava" -o "$parentArtifact" != "pom-scijava" || {
 	debug "Checking pom-scijava parent version"
 	psjMavenMetadata=https://repo1.maven.org/maven2/org/scijava/pom-scijava/maven-metadata.xml
 	latestParentVersion=$(curl -fsL "$psjMavenMetadata" | grep '<release>' | sed 's;.*>\([^<]*\)<.*;\1;')